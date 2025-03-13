23223c7001081f7cb2806f12094b498f1d533439
legojaco <legouglen@duck.com>
2025-03-13 08:05:05 +0100

    changed names, drawing lines

--- javascript.js
+++ javascript.js
@@ -1,5 +1,20 @@
 // jacob makes this //////////////////////////
+// defines canvs variables
+const canvas = document.getElementById('playboard');
+const ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
 
+// funktion to drawlines on borders
+function drawBorderLines() {
+    ctx.beginPath(); // Define a new path
 
+    for (var i = 1; i < 10; i++) {
+        ctx.moveTo((width/(10-i)), 0); //start at point
+        ctx.lineTo((width/(10-i)), height); //end at point
+        ctx.stroke(); //draw
+    }
+    ctx.stroke(); //draw
+}
 
-// lauritz makes this ////////////////////////
+drawBorderLines()
