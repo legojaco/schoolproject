a4e12aeb221c41f2c4d083b916ddac053f75cb4e
legojaco <legouglen@duck.com>
2025-03-13 08:19:29 +0100

    finished function
    to draw border lines

--- javascript.js
+++ javascript.js
@@ -10,8 +10,13 @@ function drawBorderLines() {
     ctx.beginPath(); // Define a new path
 
     for (var i = 1; i < 10; i++) {
-        ctx.moveTo((width/(10-i)), 0); //start at point
-        ctx.lineTo((width/(10-i)), height); //end at point
+        ctx.moveTo(((width/10)*i), 0); //start at point
+        ctx.lineTo(((width/10)*i), height); //end at point
+        ctx.stroke(); //draw
+    }
+    for (var i = 1; i < 20; i++) {
+        ctx.moveTo(0, ((height/20)*i)); //start at point
+        ctx.lineTo(width, ((height/20)*i)); //end at point
         ctx.stroke(); //draw
     }
     ctx.stroke(); //draw
